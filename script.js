794a6163d79d9b5c5428ed661b35806c2045f50d
José Augusto <Jose.Augusto@conninf.com.br>
2020-08-20 10:14:23 -0300

    Pagina finalizada

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let nome = window.document.getElementById('nome');
 let email = document.querySelector('#email');
 let assunto = document.querySelector('#assunto');
-
+let mapa = document.querySelector('#mapa');
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
 nome.style.width = '100%';
 email.style.width = '100%';
 
@@ -12,6 +15,7 @@ function validaNome(){
     txtNome.innerHTML='Nome Invalido';
     txtNome.style.color = 'red';
     }else{
+        nomeOk=true;
         txtNome.innerHTML='Nome Valido';
         txtNome.style.color ='green';
     }
@@ -24,7 +28,34 @@ function validaEmail(){
         txtEmail.innerHTML='Email invalido';
         txtEmail.style.color="red";
     }else{
+        emailOk=true;
         txtEmail.innerHTML='Email valido';
         txtEmail.style.color='green';
     }
+}
+function validaAssunto(){
+    let txtAssunto = document.querySelector("#txtAssunto");
+    if(assunto.value.length >= 100){
+        txtAssunto.innerHTML="Texto muito grande , digite apenas 100 caracteres";
+        txtAssunto.style.color="red";
+        txtAssunto.style.display="block";
+    }else{
+        assuntoOk=true;
+        txtAssunto.style.display ="none";
+    }
+}
+function enviar(){
+    if(nomeOk==true && emailOk==true && assuntoOk==true){
+        alert('Formulario enviado com sucesso');
+    }else{
+        alert('Preencha o formulario corretamente antes de enviar... ')
+    }
+}
+function mapaZoom() {
+    mapa.style.width="800px";
+    mapa.style.height="400px";
+}
+function mapaNormal(){
+    mapa.style.width="400px";
+    mapa.style.height="200px";
 }
